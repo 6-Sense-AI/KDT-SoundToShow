ac41a7c849414fe1706b865293bf0effc7dd3c21
jingu <jingu4055@gmail.com>
2023-06-17 19:58:41 +0900

    backup

--- S2S/mysite/kdt/views.py
+++ S2S/mysite/kdt/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .models import Audio
+from .wav_change_model import wave_change
 
 def index(request):
     audio = Audio.objects.all()
+    wave_change('test.mp3', 0.6, 25.08, 'C:/hackathon/mysite/KDT-SoundToShow/S2S/mysite/kdt/test/input', 'C:/hackathon/mysite/KDT-SoundToShow/S2S/mysite/kdt/test/output')
     print(audio)
     return render(request, 'index.html', {"audio":audio})
+
