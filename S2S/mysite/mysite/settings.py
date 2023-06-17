877f5d4a69106fd181770df578780828dac7894f
kimgitt <taesoo691@gmail.com>
2023-06-17 12:41:41 +0900

    change

--- S2S/mysite/mysite/settings.py
+++ S2S/mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = ""
+# SECRET_KEY = 'django-insecure-r+a%s$f^e3gdc5en-%!45nhyrw@=g$&7bh^rtfi!-$67_8%$d5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
