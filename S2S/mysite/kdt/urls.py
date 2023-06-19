7ff93a57e1b6557132cba75e511fd38a1c51f4a3
hnOh <dhgksmf423@gmail.com>
2023-06-20 04:01:39 +0900

    temp

--- S2S/mysite/kdt/urls.py
+++ S2S/mysite/kdt/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-app_name = 'kdt'
 urlpatterns = [
     path('', views.index, name='index'),
     path('audio', views.changeAudio, name='audio'),
--- S2S/mysite/kdt/views.py
+++ S2S/mysite/kdt/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Audio
 from . import wav_change_model
+from django.core.files.storage import FileSystemStorage
 import os
 
 def index(request):
@@ -14,10 +15,12 @@ def changeAudio(request): # 주파수 변환
     wav_change_model.wave_change('success.mp3', 0.6, 25.08, os.path.join(current_directory,'media','origin'),os.path.join(current_directory,'media','change'))
     return render(request, 'audio.html')
 
-
-
-# def test(request):
-#     wav_change_model('test', 0.6, 25.08, '.', '.')
-
-#     print(audio)
-#     return render(request, 'index.html', {"audio":audio})
+def upload_file(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['file']
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+        return render(request, 'upload.html', {
+            'message': '파일이 성공적으로 업로드되었습니다.'
+        })
+    return render(request, 'upload.html')
