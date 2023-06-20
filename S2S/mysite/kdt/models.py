f003a7cf34a863d08b39a4cab2cb3dc103e8ff0d
hnOh <dhgksmf423@gmail.com>
2023-06-21 01:49:31 +0900

    add audio code

--- S2S/mysite/kdt/models.py
+++ S2S/mysite/kdt/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Audio(models.Model):
     id = models.AutoField(primary_key=True)
-    path = models.CharField(max_length=100)
+    path = models.CharField(max_length=500)
 
     class Meta:
         managed = False
