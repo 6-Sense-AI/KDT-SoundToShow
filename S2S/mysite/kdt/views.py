05e38781988f694eb2f6687e69f42eaa8d442231
hnOh <dhgksmf423@gmail.com>
2023-06-21 19:58:49 +0900

    add result

--- S2S/mysite/kdt/views.py
+++ S2S/mysite/kdt/views.py
@@ -6,6 +6,7 @@ import os
 from django.http import HttpResponse ,JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views import View
+
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 def index(request):
@@ -14,56 +15,47 @@ def index(request):
     print(audio)
     return render(request, 'index.html', {"audio":audio})
 
-def uploadFile(request):
-    return render(request,'upload_file.html')
-
-# def changeAudio(request): # 주파수 변환
-#     current_directory = os.path.dirname(os.path.abspath(__file__)) # 현재 경로
-#     wav_change_model.wave_change('success.mp3', 0.6, 25.08, os.path.join(current_directory,'media','origin'),os.path.join(current_directory,'media','change'))
-#     return render(request, 'audio.html')
-
-
-def changeaudio(selected_frequency):
+def changeaudio(filename, selected_frequency):
     if selected_frequency:
-            current_directory = os.path.dirname(os.path.abspath(__file__))
-            origin_path = os.path.join(current_directory, 'media', 'origin')
-            change_path = os.path.join(current_directory, 'media', 'change')
+            origin_path = os.path.join(current_directory, 'media', 'sound', 'origin')
+            change_path = os.path.join(current_directory, 'media', 'sound','change')
             
             if selected_frequency == '1':   # 저주파 :20 ~ 1000
-                wav_change_model.wave_change('success.mp3', 0.3, 15, origin_path, change_path)
+                wav_change_model.wave_change(filename, "c"+filename, 0.3, 15, origin_path, change_path)
             elif selected_frequency == '2': # 저주파 :20 ~ 2000
-                wav_change_model.wave_change('success.mp3', 0.3, 25.08, origin_path, change_path)
+                wav_change_model.wave_change(filename, "c"+filename, 0.3, 25.08, origin_path, change_path)
             elif selected_frequency == '3': # 중음역 :500 ~ 3000
-                wav_change_model.wave_change('success.mp3', 7.5, 31, origin_path, change_path)
+                wav_change_model.wave_change(filename, "c"+filename, 7.5, 31, origin_path, change_path)
             elif selected_frequency == '4': # 고주파 :1000 ~ 20000
-                wav_change_model.wave_change('success.mp3', 15, 58.57, origin_path, change_path)
+                wav_change_model.wave_change(filename, "c"+filename, 15, 58.57, origin_path, change_path)
             elif selected_frequency == '5': # 고주파 :2000 ~ 20000
-                wav_change_model.wave_change('success.mp3', 25.08, 58.57, origin_path, change_path)
+                wav_change_model.wave_change(filename, "c"+filename, 25.08, 58.57, origin_path, change_path)
     return 1
 
 # 파일 업로드
 @csrf_exempt
 def upload_file(request):
     if request.method == 'POST':
-        print('$$')
         uploaded_file = request.FILES['file']
-        fs = FileSystemStorage(os.path.join(current_directory,'media','test'))
+        fs = FileSystemStorage(os.path.join(current_directory,'media','sound','origin')) # 원본 저장
 
         # 주파수 저장
         audio_id = Audio.objects.latest('id').id + 1 if Audio.objects.exists() else 1 # id 얻기
-        filename = f"{audio_id}_{uploaded_file.name}"
+        filename = f"{audio_id}_{uploaded_file.name}" # 오디오 파일 이름
         fs.save(filename, uploaded_file)
+        audio = Audio(id=audio_id, path=fs.path(filename)) #모델에 저장
+        audio.save()
 
-        # 주파수 모드 선택 값 가져오기
+        # 주파수 모드
         num = request.POST.get('num')
         print(num)
-        print(changeaudio(num))
+        print(changeaudio(filename,num)) # 파일 생성
         
-        # 입모양 생성 여부 값 가져오기
+        # 입모양 생성 여부
         generate_mouth = request.POST.get('generate_mouth')
         print(generate_mouth)
         
-        # 텍스트 생성 여부 값 가져오기
+        # 텍스트 생성 여부
         generate_text = request.POST.get('generate_text')
         print(generate_text)
 
@@ -73,27 +65,13 @@ def upload_file(request):
         else:
             file_extension = 'mp3'
 
-        audio = Audio(id=audio_id, path=fs.path(filename)) #모델에 저장
-        audio.save()
+        context = {
+            'num': num,
+            'generate_mouth': generate_mouth,
+            'generate_text': generate_text,
+        }
 
         message = '파일이 성공적으로 업로드되었습니다.'
-        return render(request, 'upload_file.html', {'message': message})
+        return render(request, 'model_result.html', context)
     return render(request, 'upload_file.html')
 
-def result():
-    return render(request, 'result.html')
-
-@csrf_exempt
-def post(request):
-    current_directory = os.path.dirname(os.path.abspath(__file__)) # 현재 경로
-    file = request.FILES.get('file')  # 클라이언트에서 전송된 파일 객체를 가져옵니다.
-    print(file)
-        # 파일 처리 및 저장 로직을 구현합니다.
-        # 예를 들어, 파일을 특정 경로에 저장하거나 필요한 처리를 수행합니다.
-        # 이 예시에서는 파일을 저장하는 대신 파일 이름을 반환합니다.
-    if file:
-        file_name = file
-        print(file_name)
-        return JsonResponse({'file_name': file_name})
-    else:
-        return JsonResponse({'error': '파일을 찾을 수 없습니다.'}, status=400)
