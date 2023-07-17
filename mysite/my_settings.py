113506c7b11711b9f10de450e62e0ddb84c23ade
haneulOh <87457244+haneul5@users.noreply.github.com>
2023-07-17 10:46:05 +0900

    Update my_settings.py
    remove mysetting

--- mysite/my_settings.py
+++ mysite/my_settings.py
@@ -7,13 +7,13 @@ DATABASES = {
         # 2. 연동할 MySQL의 데이터베이스 이름
         'NAME': 's2s',
         # 3. DB 접속 계정명  / 연결할 db에 따라 다를 수 있음
-        'USER': 'root',
+        'USER': '',
         # 4. DB 패스워드
-        'PASSWORD': 'aivle',
+        'PASSWORD': '',
         # 5. DB 주소 / 연결할 db에 따라 다를 수 있음
         'HOST': '127.0.0.1',
         # 6. 포트번호
-        'PORT': '3306',
+        'PORT': '',
     }
 }
-SECRET_KEY = "nv3shZrTkD/pJ8At/7TRMMKdTKM8p39zih2Zrlg44Zk"
+SECRET_KEY = ""
