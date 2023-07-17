1033355b016029048c6cff65955b169f1c3ae0ba
haneulOh <87457244+haneul5@users.noreply.github.com>
2023-07-17 10:35:42 +0900

    Delete S2S directory

--- S2S/mysite/kdt/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import Audio
-# Register your models here.
-
-admin.site.register(Audio)
--- S2S/mysite/kdt/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Audio(models.Model):
-    id = models.AutoField(primary_key=True)
-    path = models.CharField(max_length=500)
-
-    class Meta:
-        managed = False
-        db_table = 'audio'
